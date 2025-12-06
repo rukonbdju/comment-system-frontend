b60fa644b2e0339cc8f7d5672c4e6a3a49512b5c
rukonbd1 <rukonbd.dev@gmail.com>
2025-12-07 00:36:15 +0600

    designed login and register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,13 @@
 
 import { Button } from "@/components/shared-components/button"
 import { InputField } from "@/components/shared-components/input"
-import { Lock, Mail, MoveRight } from "lucide-react"
+import { Lock, Mail, MoveRight, User } from "lucide-react"
 import Link from "next/link"
 
 export const metadata = {
-    title: 'Login'
+    title: 'Register'
 }
-const LoginPage = () => {
+const RegisterPage = () => {
     return (
         <div className="bg-indigo-50 min-h-screen content-center">
             <div className="max-w-md bg-white mx-auto p-5 rounded-xl">
@@ -15,12 +15,18 @@ const LoginPage = () => {
                     <div className="w-12 h-12 bg-indigo-100 rounded-full flex items-center justify-center mx-auto mb-4 text-indigo-600">
                         <Lock size={24} />
                     </div>
-                    <h1 className="text-2xl font-bold text-slate-900">Welcome Back</h1>
+                    <h1 className="text-2xl font-bold text-slate-900">Register Now</h1>
                     <p className="text-slate-500 mt-2 text-sm">
-                        Sign in to join the conversation
+                        Registration first to join the conversation
                     </p>
                 </div>
                 <form className=" space-y-4">
+                    <InputField
+                        icon={<User className="size-4" />}
+                        label="Name"
+                        name="name"
+                        type="text"
+                    />
                     <InputField
                         icon={<Mail className="size-4" />}
                         label="Email"
@@ -33,13 +39,12 @@ const LoginPage = () => {
                         name="password"
                         type="password"
                     />
-                    <Button rightIcon={<MoveRight />} block={true} isLoading={false}>Login</Button>
+                    <Button rightIcon={<MoveRight />} block={true} isLoading={false}>Register</Button>
                 </form>
-                <p className="mt-4 text-sm">New user? <Link className="text-indigo-500 hover:underline font-semibold" href={'/register'}>Registration</Link></p>
-
+                <p className="mt-4 text-sm">ALready register? <Link className="text-indigo-500 hover:underline font-semibold" href={'/login'}>Login</Link></p>
             </div>
         </div>
     )
 }
 
-export default LoginPage
+export default RegisterPage
