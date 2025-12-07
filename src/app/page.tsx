5f09faad99b98915cc5f86a6179cf0d19148a5da
rukonbd1 <rukonbd.dev@gmail.com>
2025-12-07 23:42:19 +0600

    wrap layout by redux store and protect main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,23 @@
 import { MessageSquare } from 'lucide-react';
 import TopBar from '@/components/layout/topbar';
 import Comments from '@/components/comments/comments';
+import ProtectedPage from './protected-page';
 
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-50 font-inter">
-      <TopBar />
-      <div className="max-w-4xl mx-auto p-4 sm:p-6 pt-6">
-        <h1 className="text-3xl font-bold text-gray-800 mb-6 flex items-center">
-          <MessageSquare className="w-7 h-7 mr-3 text-indigo-600" />
-          Discussion
-        </h1>
-        <Comments />
+    <ProtectedPage>
+      <div className="min-h-screen bg-gray-50 font-inter">
+        <TopBar />
+        <div className="max-w-4xl mx-auto p-4 sm:p-6 pt-6">
+          <h1 className="text-3xl font-bold text-gray-800 mb-6 flex items-center">
+            <MessageSquare className="w-7 h-7 mr-3 text-indigo-600" />
+            Discussion
+          </h1>
+          <Comments />
+        </div>
       </div>
-    </div>
+    </ProtectedPage>
   );
 };
 
---
+++ src/app/protected-page.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { useAppDispatch } from "@/hooks/useAppDispatch";
+import { authSelector } from "@/lib/features/auth/auth.slice";
+import { getLoggedInUser } from "@/lib/features/auth/auth.thunks";
+import { useRouter } from "next/navigation";
+import { ReactNode, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+const ProtectedPage = ({ children }: { children: ReactNode }) => {
+    const dispatch = useAppDispatch()
+    const { isLoading, user } = useSelector(authSelector)
+    const router = useRouter()
+    useEffect(() => {
+        if (!user) dispatch(getLoggedInUser())
+    }, [user, dispatch])
+
+    useEffect(() => {
+        if (!isLoading && !user) {
+            router.replace("/login"); // redirect if not authenticated
+        }
+    }, [isLoading, user, router]);
+
+    if (isLoading || !user) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                Loading...
+            </div>
+        );
+    }
+
+    return <>{children}</>;
+}
+
+export default ProtectedPage;
